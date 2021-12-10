c67c32a0d153ed2a33b166a6e6df5dd8f7be36f7
TatianaCarpio <tcarpio0@uoc.edu>
2021-12-10 23:00:17 +0100

    PEC_2

---
+++ dist/index.1625f28e.js
@@ -0,0 +1,7 @@
+var reply = prompt("Hola ¿cuál es tu nombre?", "");
+alert("¡Me alegra mucho de que estés aquí, " + reply + "!");
+function popup() {
+    alert("¡PATA DE CABRA!");
+}
+
+//# sourceMappingURL=index.1625f28e.js.map
